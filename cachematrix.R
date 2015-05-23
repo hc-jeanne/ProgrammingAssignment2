05d5d78bc861df8f3eeb1dbf4c1a4b4460f4c8ea
hc-jeanne <huichenjyu@yahoo.com>
2015-05-24 01:00:32 +0800

    Place my solution in this file which is the stub R file at https://github.com/rdpeng/ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache the inverse of a matrix
+## 
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+    iv <- NULL           # the inverse of the matrix 'x'
+    
+    set <- function(y) { # set the value of the matrix
+        x <<- y          # the matrix has changed , then clear cache (the inverse)
+        iv <<- NULL
+    }
 
+    get <- function() x  # get the value of the matrix
+    setInverse <- function(inverse) iv <<- inverse  # set the value of the inverse
+    getInverse <- function() iv                     # get the value of the inverse
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    iv <- x$getInverse()    # get the inverse in x (a makeCacheMatrix)
+    
+    # check if the inverse has calculated :
+    if (!is.null(iv)) {     # the inverse has already been calculated
+        message("getting cached data")
+        return(iv)          # return the cached inverse
+    }
+    # the inverse has not been calculated yet : then compute it:
+    data <- x$get()         # get the matrix
+    iv <- solve(data)       # calculate the inverse of the matrix (a square matrix)
+    x$setInverse(iv)        # cache the calculated inverse
+    iv                      # Return the inverse (a matrix that is the inverse of 'x')
 }
